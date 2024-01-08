b2d261f605cae8b4e0795b3494f3e35ddd51fb4c
saurabh <saurabh@saurabhdavda7799.com>
2024-01-08 22:46:12 +0530

    added

---
+++ 07mathfunction.py
@@ -0,0 +1,16 @@
+import math
+
+pi = 3.14
+a = -1
+x = 1
+y = 2
+z = 3
+
+print(round(pi))
+print(math.ceil(pi)) #rounding integer up
+print(math.floor(pi)) #rounding integer down
+print(abs(a)) #positive value
+print(pow(pi,2)) #power function
+print(math.sqrt(25))
+print(max(x,y,z)) 
+print(min(x,y,z)) 
