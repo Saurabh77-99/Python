e9203d1e9dbb6b3fe0a47d1444fd0a09b0d51a33
saurabh <saurabh@saurabhdavda7799.com>
2024-01-08 22:25:46 +0530

    added

---
+++ 06userinput.py
@@ -0,0 +1,8 @@
+# default input is of string datatype
+name = input("What is your name? :")
+age = int(input("How old are you?:"))
+height = float(input("what is your height in cm?:"))
+
+print("Hello " + name)
+print("Your age is " + str(age)+" years old")
+print("Your height is " + str(height)+" cm short")
