074fba6bd79597374e64703d76cf8398e2f1831b
saurabh <saurabh@saurabhdavda7799.com>
2024-01-10 16:23:44 +0530

    added

---
+++ 16-2Dlists.py
@@ -0,0 +1,16 @@
+# 2D lists = a list of lists
+
+drinks = ["coffee","tea","soda"]
+dinner =["pizza","burger","puff"]
+dessert =["brownie","red-velvet","black-current"]
+
+food = [drinks,dinner,dessert]
+
+print(food[0]) #will print first list of food list
+print()
+print(food[0][0]) # first element of first list
+print()
+print(food[0][1]) # second element of first list
+print()
+print(food[2][2]) # third element of second list
+print()
